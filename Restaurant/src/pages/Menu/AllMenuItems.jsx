bb068b809d216a6918ae4592401b36f16618f156
dhruvmathur2001@gmail.com <dhruvmathur2001@gmail.com>
2024-10-24 11:54:57 +0530

    next commit

---
+++ Restaurant/src/pages/Menu/AllMenuItems.jsx
@@ -0,0 +1,152 @@
+//Salad1
+import Salad1 from "./Imgs/Salad1.jpg";
+import CaeserSalad from "./Imgs/CaeserSalad.webp";
+import Capsal from "./Imgs/Capsal.webp";
+import Cobb from "./Imgs/Cobb.jpg";
+import Spinach from "./Imgs/SpinachS.jpg";
+import Chana from "./Imgs/Chana.jpg";
+import Waldorf from "./Imgs/Waldorf.webp";
+import SW from "./Imgs/SW.jpg";
+import Quiona from "./Imgs/Quiona.jpg";
+import CS from "./Imgs/CS.jpg";
+import Avo from "./Imgs/Avocado.avif";
+import Fruit from "./Imgs/Fruit.jpg";
+
+const SaladItems1 = [
+  { name: "Caesar Salad", rate: 1, src: Salad1 },
+  { name: "Greek Salad", rate: 2, src: CaeserSalad },
+  { name: "Caprese Salad", rate: 3, src: Capsal },
+  { name: "Cobb Salad", rate: 4, src: Cobb },
+  { name: "Spinach Salad", rate: 5, src: Spinach },
+  { name: "Chana Chaat", rate: 6, src: Chana },
+  { name: "Waldorf Salad", rate: 7, src: Waldorf },
+  { name: "SouthWest Salad", rate: 8, src: SW },
+  { name: "Quinoa Salad", rate: 9, src: Quiona },
+  { name: "Caprese Salad", rate: 10, src: CS },
+  { name: "Avocado Salad", rate: 11, src: Avo },
+  { name: "Fruit Salad", rate: 12, src: Fruit },
+];
+
+//Starters
+
+import PT from "./StarterImg/PaneerTikka.jpeg";
+import VP from "./StarterImg/VP.jpeg";
+import AlooT from "./StarterImg/AlooT.webp";
+import Hara from "./StarterImg/Hara.jpeg";
+import DP from "./StarterImg/DP.jpg";
+import SM from "./StarterImg/SM.jpg";
+import SPR from "./StarterImg/SPR.webp";
+import CSB from "./StarterImg/CSB.jpg";
+import Bhel from "./StarterImg/Bhel.jpg";
+import ChilliP from "./StarterImg/ChilliP.jpeg";
+import GolG from "./StarterImg/GolG.webp";
+import PK from "./StarterImg/PK.webp";
+
+let StarterItems = [
+  { name: "Paneer-Tikka", rate: 1, src: PT },
+  { name: "Vegetable Pakoda", rate: 2, src: VP },
+  { name: "Aloo Tikki", rate: 3, src: AlooT },
+  { name: "Hara Bhara Kabab", rate: 4, src: Hara },
+  { name: "Dahi Puri", rate: 5, src: DP },
+  { name: "Stuff Mushrooms", rate: 6, src: SM },
+  { name: "Vegetable Spring Rolls", rate: 7, src: SPR },
+  { name: "Corn Cheese Balls", rate: 8, src: CSB },
+  { name: "Bhel Puri", rate: 9, src: Bhel },
+  { name: "Chilli Paneer", rate: 10, src: ChilliP },
+  { name: "Pani Puri", rate: 11, src: GolG },
+  { name: "Palak Chaat", rate: 12, src: PK },
+];
+
+//DrinksItems
+import MC from "./DrinksImg/MC.jpg";
+import Lassi from "./DrinksImg/Lassi.jpg";
+import JJ from "./DrinksImg/JJ.webp";
+import CSh from "./DrinksImg/CSh.jpg";
+import ButterS from "./DrinksImg/ButterS.jpeg";
+import VM from "./DrinksImg/VM.jpeg";
+import MSC from "./DrinksImg/MSC.jpeg";
+import IC from "./DrinksImg/IC.jpg";
+import CW from "./DrinksImg/CW.webp";
+import ThumpsUp from "./DrinksImg/ThumpsUp.jpeg";
+import LimeWater from "./DrinksImg/LimeWater.jpeg";
+import Chaas from "./DrinksImg/Chaas.webp";
+
+let DrinksItems = [
+  { name: "Masala Chai", rate: 1, src: MC },
+  { name: "Lassi", rate: 2, src: Lassi },
+  { name: "Jaljeera", rate: 3, src: JJ },
+  { name: "Chocolate Shake ", rate: 4, src: CSh },
+  { name: "Butterscotch Shake", rate: 5, src: ButterS },
+  { name: "Virgin Mojito", rate: 6, src: VM },
+  { name: "Mango Mint Cooler", rate: 7, src: MSC },
+  { name: "Ice Coffee", rate: 8, src: IC },
+  { name: "Tender Coconut Water", rate: 9, src: CW },
+  { name: "Thumps Up", rate: 10, src: ThumpsUp },
+  { name: "Lime Water: Salt/Sugar/Mix", rate: 11, src: LimeWater },
+  { name: "Buttermilk (Chaas)", rate: 12, src: Chaas },
+];
+
+//LunchItems
+
+import AlooG from "./LunchImg/Alooghobi.jpg";
+import ChanaM from "./LunchImg/ChanaM.jpeg";
+import DalM from "./LunchImg/DalM.jpeg";
+import DumAloo from "./LunchImg/DumAloo.jpeg";
+import MalaiK from "./LunchImg/MalaiK.jpeg";
+import PalakP from "./LunchImg/PalakP.jpg";
+import PBM from "./LunchImg/PBM.jpg";
+import Rajma from "./LunchImg/Rajma.jpeg";
+import Rice from "./LunchImg/Rice.jpg";
+import Biryani from "./LunchImg/Biryani.jpg";
+import AlooP from "./LunchImg/AlooP.jpg";
+import Roti from "./LunchImg/Roti.jpeg";
+
+let LunchItems = [
+  { name: "Aloo Ghobi", rate: 1, src: AlooG },
+  { name: "Chana Masala", rate: 2, src: ChanaM },
+  { name: "Dal Makhni", rate: 3, src: DalM },
+  { name: "Dum Aloo ", rate: 4, src: DumAloo },
+  { name: "Malai Kofta", rate: 5, src: MalaiK },
+  { name: "Palak Paneer", rate: 6, src: PalakP },
+  { name: "Paneer Butter Masala", rate: 7, src: PBM },
+  { name: "Rajma", rate: 8, src: Rajma },
+  { name: "Rice", rate: 9, src: Rice },
+  { name: "Vegetable Biryani", rate: 10, src: Biryani },
+  { name: "Aloo Paratha", rate: 11, src: AlooP },
+  { name: "Roti", rate: 12, src: Roti },
+];
+
+//DESSERT
+import Gulab from "./DessertImg/Gulab.webp";
+import Rasmalai from "./DessertImg/RasaMalai.jpeg";
+import Jalebi from "./DessertImg/Jalebi.jpeg";
+import Kheer from "./DessertImg/Kheer.jpeg";
+import Gajar from "./DessertImg/Gajar.jpeg";
+import Rasgulla from "./DessertImg/Rasgulla.jpg";
+import Malpua from "./DessertImg/Malpua.jpg";
+import MysorePak from "./DessertImg/MysorePak.jpeg";
+import Kulfi from "./DessertImg/Kulfi.webp";
+import Ice from "./DessertImg/ICream.jpeg";
+import Balu from "./DessertImg/Balu.jpeg";
+import Rabdi from "./DessertImg/Rabdi.jpg";
+
+let DessertItems = [
+  { name: "Gulab Jamun", rate: 1, src: Gulab },
+  { name: "Rasmalai", rate: 2, src: Rasmalai },
+  { name: "Jalebi", rate: 3, src: Jalebi },
+  { name: "Kheer", rate: 4, src: Kheer },
+  { name: "Gajar ka Halwa", rate: 5, src: Gajar },
+  { name: "Rasgulla", rate: 6, src: Rasgulla },
+  { name: "Malpua", rate: 7, src: Malpua },
+  { name: "Mysore Pak", rate: 8, src: MysorePak },
+  { name: "Kulfi", rate: 9, src: Kulfi },
+  {
+    name: "Ice Cream - Stawberry/ButterScotch/Vanilla/Chocolate",
+    rate: 10,
+    src: Ice,
+  },
+  { name: "BaluShahi", rate: 11, src: Balu },
+  { name: "Rabri", rate: 12, src: Rabdi },
+];
+
+export { SaladItems1, StarterItems, DrinksItems, LunchItems, DessertItems };
