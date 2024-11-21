77e1f87526bf66d07753b1bb78527822622d5dcd
Harsh Mathur <powerharsh2004@gmail.com>
2024-11-21 16:20:41 +0530

    minute change

--- Restaurant/src/pages/OrderHere/SearchItems.jsx
+++ Restaurant/src/pages/OrderHere/SearchItems.jsx
@@ -8,13 +8,13 @@ import { useContext } from "react";
 import { CartContext } from "../../StoreContext/Storecontext";
 
 const SearchItems = () => {
-  const {isLoggedIn} = useContext(CartContext)
+  const {isLoggedIn,user} = useContext(CartContext)
   return (
     <>
     {isLoggedIn ? (<>
       <div className={`${styles.FoodSelection_Proceed}`}>
         <div className={`${styles.SearchBar_Menu}`}>
-            <p className={styles.yourcart}>YOUR CART</p>
+            <p className={styles.yourcart}>YOUR CART <span className={styles.UnameCart}>{user?.uname}</span></p>
         </div>
         <div className={`${styles.MenuItemsContainer}`}>
           <MenuContainer />
